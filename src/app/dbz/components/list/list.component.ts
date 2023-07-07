cef70dde130f37ffd6f4439545598883ad600548
Caleste Nahir Sanchez <cesanchez@ams.red>
2023-07-07 15:08:27 -0300

    Docs creado

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interface/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Input()
+  public characterList :Character [] = [{
+    name:'Trunk',
+    power:10,
+  }]
+ /*  onDeleteId */
+ @Output()
+ public onDelete: EventEmitter<string> = new EventEmitter();
+
+ onDeleteCharacter(id?: string):void{
+  if(!id) return;
+  this.onDelete.emit(id);
+ }
+
+  }
+
